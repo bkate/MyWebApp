563b35330b7a769d6c99164d0539f39f89e13460
bkate <kaoutare.bedda@haw-hamburg.de>
2015-10-22 11:26:46 +0200

    @bkate first push to remote repo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAppAngularJsApp
+ */
+angular.module('myAppAngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
